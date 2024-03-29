b9052505df9354b42bca8f31b0228923d385795f
Lucrezia <ateossg5@gmail.com>
2024-03-29 22:22:42 +0100

    turtle files update

--- ttl files/rdf_graph.py
+++ ttl files/rdf_graph.py
@@ -10,6 +10,6 @@ ex = Namespace("http://cleopatra-project.org/")
 g.add((ex.subject1, ex.predicate1, Literal("Object 1")))
 g.add((ex.subject2, ex.predicate2, Literal("Object 2")))
 
-# Serialize the graph to a file
+# Serialize the graph into a file
 file_path = "output.rdf"
 g.serialize(destination=file_path, format="turtle")
